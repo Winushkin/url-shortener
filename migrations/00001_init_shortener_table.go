0da47f3b071609af6e563f1a244a9675b65f18ad
Kirill Belyaev <kirill10121415@gmail.com>
2026-06-06 18:33:22 +0300

    feat: init urls table

--- migrations/00001_init_shortener_table.go
+++ migrations/00001_init_shortener_table.go
@@ -19,8 +19,8 @@ func upInitShortenerTable(ctx context.Context, tx *sql.Tx) error {
 			id BIGSERIAL PRIMARY KEY,
 			long_url TEXT NOT NULL,
 			short_code VARCHAR(10) UNIQUE,
-			created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-			clicks_count BIGINT DEFAULT 0
+			clicks_count BIGINT DEFAULT 0,
+			created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 		);
 
 		CREATE INDEX IF NOT EXISTS idx_urls_short_code ON urls(short_code);			
