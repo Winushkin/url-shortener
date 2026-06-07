568ce72265b9c28947760a4e2deea2eabd3b1667
Kirill Belyaev <kirill10121415@gmail.com>
2026-06-07 22:26:00 +0300

    feat: add handler and decompose main()

--- cmd/main.go
+++ cmd/main.go
@@ -4,14 +4,21 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"net"
+	"net/http"
 
 	"github.com/Winushkin/go-toolkit/config"
 	"github.com/Winushkin/go-toolkit/logger"
 	"github.com/Winushkin/go-toolkit/postgres"
+	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/jackc/pgx/v5/stdlib"
 	"github.com/pressly/goose/v3"
 	"go.uber.org/zap"
 
+	"shortener/internal/http/handler"
+	"shortener/internal/http/middleware"
+	repository "shortener/internal/repository/postgres"
+	"shortener/internal/usecase"
 	_ "shortener/migrations"
 )
 
@@ -33,15 +40,61 @@ func main() {
 	cfg := config.NewAppConfig()
 
 	// БД пул
-	pool, err := postgres.NewPool(ctx, cfg.Postgres)
+	pool := connectDB(ctx, cfg.Postgres)
+	defer pool.Close()
+
+	// Миграции
+	migrate(ctx, pool)
+
+	repo := repository.NewPostgres(pool)
+	uc := usecase.NewURLUseCase(repo)
+	handler := handler.NewHandler(uc, cfg.DomainName)
+
+	// Cервер
+	server := registerServer(ctx, handler, cfg.Port)
+
+	log.Info(ctx, "Server is running", zap.String("Port", cfg.Port))
+	if err := server.ListenAndServe(); err != nil {
+		log.Error(ctx, err, "Server failed")
+	}
+}
+
+func registerServer(ctx context.Context, handler *handler.Handler, port string) *http.Server {
+	mux := http.NewServeMux()
+	handler.RegisterRouters(mux)
+	wrappedMux := middleware.LoggingMiddleware(mux)
+
+	server := &http.Server{
+		Addr:    ":" + port,
+		Handler: wrappedMux,
+		BaseContext: func(l net.Listener) context.Context {
+			return ctx
+		},
+	}
+
+	return server
+}
+
+func connectDB(ctx context.Context, cfg postgres.Config) *pgxpool.Pool {
+	log, ok := logger.GetLoggerFromCtx(ctx)
+	if !ok {
+		panic("logger not found in context")
+	}
+
+	pool, err := postgres.NewPool(ctx, cfg)
 	if err != nil {
 		panic(fmt.Errorf("failed to create postgres pool: %w", err))
 	}
-	defer pool.Close()
+	log.Info(ctx, "Успешное подключение к базе данных!", zap.String("Port", cfg.Port))
+	return pool
+}
 
-	log.Info(ctx, "Успешное подключение к базе данных!", zap.String("Port", cfg.Postgres.Port))
+func migrate(ctx context.Context, pool *pgxpool.Pool) {
+	log, ok := logger.GetLoggerFromCtx(ctx)
+	if !ok {
+		panic("logger not found in context")
+	}
 
-	// Миграции
 	if err := goose.SetDialect("postgres"); err != nil {
 		log.Error(ctx, err, "Ошибка настройки диалекта")
 	}
@@ -54,9 +107,8 @@ func main() {
 	}(db)
 
 	log.Info(ctx, "Запуск миграций базы данных")
-	if err := goose.Up(db, ""); err != nil {
+	if err := goose.Up(db, "."); err != nil {
 		log.Error(ctx, err, "Ошибка выполнения миграций")
 	}
 	log.Info(ctx, "Миграции успешно применены!")
-
 }
