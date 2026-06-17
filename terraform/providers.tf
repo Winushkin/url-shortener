999d5e2acde432471fb01f59056cf673342e53fc
Kirill Belyaev <kirill10121415@gmail.com>
2026-06-17 20:43:18 +0300

    feat: add terraform

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,16 +1,29 @@
 terraform {
+  required_version = ">= 1.3.0"
+
   required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = ">= 0.100.0"
+    }
     kafka = {
       source  = "Mongey/kafka"
-      version = "0.7.1" 
+      version = "~> 0.7.0"
     }
+  }
+
+  # Хранение состояния в S3-бакете Yandex Cloud
+  backend "s3" {
+    endpoints = { s3 = "https://storage.yandexcloud.net" }
+    bucket   = "yc-url-shortener-tfstate" 
+    key      = "prod/terraform.tfstate"   
+    region   = "ru-central1"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true 
   }
 }
 
-provider "kafka" {
-  bootstrap_servers = ["localhost:9094"]
-  
-  # Оставляем только эти параметры для локального подключения без шифрования
-  tls_enabled = false
-  timeout     = 10
-}
+# Инициализация провайдера Yandex Cloud
+provider "yandex" {}
